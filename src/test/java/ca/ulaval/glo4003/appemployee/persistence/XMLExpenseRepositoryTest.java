454719ea901b38975f07c5bc7bbe4c375e2b4fe8
Marie-Pier Pouliot-Lavoie <plavoie.mariepier@gmail.com>
2014-10-18 11:14:50 -0400

    test mongol

--- src/test/java/ca/ulaval/glo4003/appemployee/persistence/XMLExpenseRepositoryTest.java
+++ src/test/java/ca/ulaval/glo4003/appemployee/persistence/XMLExpenseRepositoryTest.java
@@ -3,44 +3,48 @@ package ca.ulaval.glo4003.appemployee.persistence;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.joda.time.LocalDate;
 import org.junit.Test;
 
 import ca.ulaval.glo4003.appemployee.domain.expense.Expense;
+import ca.ulaval.glo4003.appemployee.domain.user.Role;
 import ca.ulaval.glo4003.appemployee.domain.user.User;
 
 public class XMLExpenseRepositoryTest {
 
 	private static final String USER_ID = "employee@employee.com";
 	private static final double AMOUNT = 100.00;
+	private static final double WAGE = 70.00;
+	private static final Role ROLE = Role.EMPLOYEE;
+	private static final String PASSWORD = "employee";
 
 	private XMLUserRepository xmlUserRepositoryMock;
 	private XMLExpenseRepository xmlExpenseRepository;
 	private Expense expenseMock;
 	private User userMock;
+	
+	List<Expense> foundExpenses = new ArrayList<Expense>();
 
 	public void init() throws Exception {
 		xmlExpenseRepository = new XMLExpenseRepository();
-		expenseMock = mock(Expense.class);
+		expenseMock = new Expense();
+		expenseMock.setAmount(AMOUNT);
+		expenseMock.setUserEmail(USER_ID);
+		expenseMock.setDate(new LocalDate("2014-10-10"));
+		expenseMock.setuId(USER_ID);
 		xmlUserRepositoryMock = mock(XMLUserRepository.class);
-		userMock = mock(User.class);
-		createUser();
+		userMock = new User(USER_ID, PASSWORD, ROLE, WAGE);
 	}
 
 	@Test
 	public void findAllExpensesForUserReturnsListOfExpenses() throws Exception {
-		when(expenseMock.getUserEmail()).thenReturn(USER_ID);
-		when(expenseMock.getAmount()).thenReturn(AMOUNT);
-		
-		xmlExpenseRepository.store(expenseMock);
-		
+		when(xmlUserRepositoryMock.findByEmail(USER_ID)).thenReturn(userMock);
+
 		List<Expense> sampleList = xmlExpenseRepository.findAllExpensesByUser(USER_ID);
+		
 		assertTrue(sampleList.contains(expenseMock));
-	}
-	
-	public void createUser() throws Exception{
-		userMock.setEmail(USER_ID);
-		xmlUserRepositoryMock.store(userMock);
 	}
 }
