067758987736073aa2d6b2c90073656b593ec680
Marie-Pier Pouliot-Lavoie <plavoie.mariepier@gmail.com>
2014-10-18 12:13:41 -0400

    test fixed yayyy

--- src/test/java/ca/ulaval/glo4003/appemployee/persistence/XMLExpenseRepositoryTest.java
+++ src/test/java/ca/ulaval/glo4003/appemployee/persistence/XMLExpenseRepositoryTest.java
@@ -1,40 +1,39 @@
-//package ca.ulaval.glo4003.appemployee.persistence;
-//
-//import java.util.List;
-//
-//import org.junit.*;
-//
-//import static org.junit.Assert.*;
-//import static org.mockito.Mockito.*;
-//import ca.ulaval.glo4003.appemployee.domain.expense.Expense;
-//import ca.ulaval.glo4003.appemployee.domain.user.User;
-//
-//public class XMLExpenseRepositoryTest {
-//	
-//	private static final String USER_EMAIL = "employee@employee.com";
-//	
-//	private XMLExpenseRepository xmlExpenseRepository;
-//	private XMLUserRepository xmlUserRepositoryMock;
-//	private Expense expenseMock;
-//	private User userMock;
-//	
-//	@Before
-//	public void init() throws Exception{
-//		expenseMock = mock(Expense.class);
-//		userMock = mock(User.class);
-//		xmlUserRepositoryMock = mock(XMLUserRepository.class);
-//		xmlExpenseRepository = new XMLExpenseRepository();
-//	}
-//	
-//	@Test
-//	public void findAllExpensesByUserReturnsExpensesList(){
-//		when(expenseMock.getUserEmail()).thenReturn(USER_EMAIL);
-//		when(xmlUserRepositoryMock.findByEmail(USER_EMAIL)).thenReturn(userMock);
-//		
-//		List<Expense> sampleExpenseList = xmlExpenseRepository.findAllExpensesByUser(USER_EMAIL);
-//		
-//		assertTrue(sampleExpenseList.contains(expenseMock));
-//	}
-//	
-//
-//}
+package ca.ulaval.glo4003.appemployee.persistence;
+
+import java.util.List;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import ca.ulaval.glo4003.appemployee.domain.expense.Expense;
+import ca.ulaval.glo4003.appemployee.domain.user.User;
+
+public class XMLExpenseRepositoryTest {
+	
+	private static final String USER_EMAIL = "employee@employee.com"; //employee a 2 expenses dans le repo
+	private static final Integer LIST_SIZE = 2;
+	private static final double EPSILON = 0.001;
+	
+	private XMLExpenseRepository xmlExpenseRepository;
+	private XMLUserRepository xmlUserRepositoryMock;
+	private User userMock;
+	
+	@Before
+	public void init() throws Exception{
+		userMock = mock(User.class);
+		xmlUserRepositoryMock = mock(XMLUserRepository.class);
+		xmlExpenseRepository = new XMLExpenseRepository();
+	}
+	
+	@Test
+	public void findAllExpensesByUserReturnsExpensesList(){
+		when(xmlUserRepositoryMock.findByEmail(USER_EMAIL)).thenReturn(userMock);
+		
+		List<Expense> sampleExpenseList = xmlExpenseRepository.findAllExpensesByUser(USER_EMAIL);
+		
+		assertEquals(sampleExpenseList.size(), LIST_SIZE, EPSILON);
+	}
+	
+
+}
