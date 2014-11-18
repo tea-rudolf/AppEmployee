e6224d6a48f7cd9ae2d93def3b3f86bce2159950
Frédéric Joncas <abou1_2@hotmail.com>
2014-11-17 20:06:16 -0500

    createTravel TESTED like a BAWSS - we need MOAR coverage

--- src/test/java/ca/ulaval/glo4003/appemployee/services/TravelServiceTest.java
+++ src/test/java/ca/ulaval/glo4003/appemployee/services/TravelServiceTest.java
@@ -55,4 +55,10 @@ public class TravelServiceTest {
 		verify(travelMock, times(1)).setUid(NEW_UID);
 	}
 
+	@Test
+	public void createCallsCorrectRepositoryMethod() throws Exception {
+		when(travelConverterMock.convert(travelViewModelMock)).thenReturn(travelMock);
+		travelService.createTravel(travelViewModelMock);
+		verify(travelRepositoryMock, times(1)).store(travelMock);
+	}
 }
