a5322f69004e9ef725cc941e2a2366562637033d
Marie-Pier Pouliot-Lavoie <plavoie.mariepier@gmail.com>
2014-11-17 20:20:01 -0500

    Tests de DepartmentController (qui était à 19%......)

--- src/test/java/ca/ulaval/glo4003/appemployee/web/controllers/DepartmentControllerTest.java
+++ src/test/java/ca/ulaval/glo4003/appemployee/web/controllers/DepartmentControllerTest.java
@@ -2,6 +2,8 @@ package ca.ulaval.glo4003.appemployee.web.controllers;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
@@ -12,6 +14,10 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import ca.ulaval.glo4003.appemployee.domain.department.Department;
+import ca.ulaval.glo4003.appemployee.domain.exceptions.DepartmentNotFoundException;
+import ca.ulaval.glo4003.appemployee.domain.user.Role;
+import ca.ulaval.glo4003.appemployee.domain.user.User;
 import ca.ulaval.glo4003.appemployee.services.DepartmentService;
 import ca.ulaval.glo4003.appemployee.services.UserService;
 import ca.ulaval.glo4003.appemployee.web.converters.DepartmentConverter;
@@ -24,6 +30,12 @@ public class DepartmentControllerTest {
 	private static final String EMAIL_ATTRIBUTE = "email";
 	private static final String DEPARTMENT_NAME = "dummyDepartment";
 	private static final String EMAIL = "test@test.com";
+	private static final String DEPARTMENT_FORM = "editDepartment";
+	private static final String CREATE_USER_FORM = "createUser";
+	private static final String EDIT_DEPARTMENT_REDIRECT = "redirect:/departments/{departmentName}/edit";
+	private static final String EDIT_EMPLOYEE_FORM = "editEmployee";
+	
+	private List<User> users = new ArrayList<User>();
 
 	@Mock
 	private DepartmentService departmentServiceMock;
@@ -45,6 +57,12 @@ public class DepartmentControllerTest {
 
 	@Mock
 	private UserViewModel userViewModelMock;
+	
+	@Mock
+	private Department departmentMock;
+	
+	@Mock 
+	private User userMock;
 
 	@InjectMocks
 	private DepartmentController departmentController;
@@ -75,6 +93,15 @@ public class DepartmentControllerTest {
 		String form = departmentController.showEmployeesList(DEPARTMENT_NAME, modelMock, sessionMock);
 		assertEquals(REDIRECT_LINK, form);
 	}
+	
+	@Test
+	public void showEmployeesListReturnEditedDepartmentForm() throws DepartmentNotFoundException{
+		when(sessionMock.getAttribute(EMAIL_ATTRIBUTE)).thenReturn(EMAIL);
+		when(departmentServiceMock.retrieveDepartmentByName(DEPARTMENT_NAME)).thenReturn(departmentMock);
+		when(departmentServiceMock.retrieveEmployeesList(DEPARTMENT_NAME)).thenReturn(users);
+		String returnedForm = departmentController.showEmployeesList(DEPARTMENT_NAME, modelMock, sessionMock);
+		assertEquals(DEPARTMENT_FORM, returnedForm);
+	}
 
 	@Test
 	public void showCreateEmployeeAccountPageRedirectsToLoginPageWhenSessionAttributeIsNull() throws Exception {
@@ -82,5 +109,60 @@ public class DepartmentControllerTest {
 		String form = departmentController.showCreateEmployeeAccountPage(DEPARTMENT_NAME, modelMock, userViewModelMock, sessionMock);
 		assertEquals(REDIRECT_LINK, form);
 	}
+	
+	@Test
+	public void showCreateEmployeeAccountPageReturnsUserCreationFormWhenSuccessful(){
+		when(sessionMock.getAttribute(EMAIL_ATTRIBUTE)).thenReturn(EMAIL);
+		String returnedForm = departmentController.showCreateEmployeeAccountPage(DEPARTMENT_NAME, modelMock, userViewModelMock, sessionMock);
+		assertEquals(CREATE_USER_FORM, returnedForm);
+	}
+	
+	@Test
+	public void createEmployeeAccountReturnsAccountCreationFormWhenRoleIsMissing(){
+		when(sessionMock.getAttribute(EMAIL_ATTRIBUTE)).thenReturn(EMAIL);
+		when(userViewModelMock.getRole()).thenReturn("NONE");
+		String returnedForm = departmentController.createEmployeeAccount(DEPARTMENT_NAME, modelMock, userViewModelMock, sessionMock);
+		assertEquals(CREATE_USER_FORM, returnedForm);
+	}
+	
+	@Test
+	public void createEmployeeAccountRedirectsToEditionPageWhenRoleIsNotMissing(){
+		when(sessionMock.getAttribute(EMAIL_ATTRIBUTE)).thenReturn(EMAIL);
+		when(userViewModelMock.getRole()).thenReturn(Role.EMPLOYEE.toString());
+		String returnedForm = departmentController.createEmployeeAccount(DEPARTMENT_NAME, modelMock, userViewModelMock, sessionMock);
+		assertEquals(EDIT_DEPARTMENT_REDIRECT, returnedForm);
+	}
+	
+	@Test
+	public void showUpdateEmployeeInfoRedirectsWhenSessionAttributeIsMissing(){
+		when(sessionMock.getAttribute(EMAIL_ATTRIBUTE)).thenReturn(null);
+		String returnedForm = departmentController.showUpdateEmployeeInfo(DEPARTMENT_NAME, EMAIL, modelMock, sessionMock);
+		assertEquals(REDIRECT_LINK, returnedForm);
+	}
+	
+	@Test
+	public void showUpdateEmployeeInfoReturnsEditEmployeeFormWhenSuccessful(){
+		when(sessionMock.getAttribute(EMAIL_ATTRIBUTE)).thenReturn(EMAIL);
+		when(userServiceMock.retrieveByEmail(EMAIL)).thenReturn(userMock);
+		String returnedForm = departmentController.showUpdateEmployeeInfo(DEPARTMENT_NAME, EMAIL, modelMock, sessionMock);
+		assertEquals(EDIT_EMPLOYEE_FORM, returnedForm);
+	}
+	
+	@Test
+	public void updateEmployeeInfoRedirectsToUserCreationPageIfRoleIsMissing(){
+		when(sessionMock.getAttribute(EMAIL_ATTRIBUTE)).thenReturn(EMAIL);
+		when(userViewModelMock.getRole()).thenReturn("NONE");
+		String returnedForm = departmentController.updateEmployeeInfo(DEPARTMENT_NAME, userViewModelMock, modelMock, sessionMock);
+		assertEquals(CREATE_USER_FORM, returnedForm);
+	}
+	
+	@Test
+	public void updateEmployeeInfoReturnsEditedDepartmentFormIfSuccessful(){
+		when(sessionMock.getAttribute(EMAIL_ATTRIBUTE)).thenReturn(EMAIL);
+		when(userViewModelMock.getRole()).thenReturn(Role.EMPLOYEE.toString());
+		String returnedForm = departmentController.updateEmployeeInfo(DEPARTMENT_NAME, userViewModelMock, modelMock, sessionMock);
+		assertEquals(EDIT_DEPARTMENT_REDIRECT, returnedForm);
+	}
+	
 
 }
