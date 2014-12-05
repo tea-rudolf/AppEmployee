850fb2f32de48cdb9b87d4e76bbe82420f2e8d1c
Marie-Pier Pouliot-Lavoie <plavoie.mariepier@gmail.com>
2014-12-05 18:04:12 -0500

    TravelServiceTest

--- src/test/java/ca/ulaval/glo4003/appemployee/services/TravelServiceTest.java
+++ src/test/java/ca/ulaval/glo4003/appemployee/services/TravelServiceTest.java
@@ -1,23 +1,39 @@
 package ca.ulaval.glo4003.appemployee.services;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.joda.time.LocalDate;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import ca.ulaval.glo4003.appemployee.domain.exceptions.TravelNotFoundException;
 import ca.ulaval.glo4003.appemployee.domain.repository.TravelRepository;
 import ca.ulaval.glo4003.appemployee.domain.travel.Travel;
 import ca.ulaval.glo4003.appemployee.domain.travel.TravelProcessor;
+import ca.ulaval.glo4003.appemployee.domain.travel.Vehicle;
 import ca.ulaval.glo4003.appemployee.web.converters.TravelConverter;
 import ca.ulaval.glo4003.appemployee.web.viewmodels.TravelViewModel;
 
 public class TravelServiceTest {
 
-	// TODO: fix the tests
+	private static final double DISTANCE_TRAVELLED = 200.00;
+	private static final Vehicle USER_VEHICLE = Vehicle.PERSONAL;
+	private static final String TRAVEL_DATE = "2014-11-11";
+	private static final String VALID_EMAIL = "employee@employee.com";
+	private static final String COMMENT = "comment";
+	private static final String TRAVEL_UID = "1";
 
-	private static final String TRAVEL_UID = "0002";
-	private static final String NEW_UID = "0003";
-
+	private List<Travel> travels = new ArrayList<Travel>();
+	
 	@Mock
 	private TravelRepository travelRepositoryMock;
 
@@ -46,17 +62,44 @@ public class TravelServiceTest {
 				payPeriodServiceMock, travelProcessorMock);
 	}
 
-//	 @Test
-//	 public void updateCallsCorrectDomainMethod() {
-//	 when(travelConverterMock.convert(travelViewModelMock)).thenReturn(travelMock);
-//	 travelService.updateTravel(NEW_UID, travelViewModelMock);
-//	 verify(travelMock, times(1)).setUid(NEW_UID);
-//	 }
-//
-//	 @Test
-//	 public void createCallsCorrectRepositoryMethod() throws Exception {
-//	 when(travelConverterMock.convert(travelViewModelMock)).thenReturn(travelMock);
-//	 travelService.createTravel(travelViewModelMock);
-//	 verify(travelRepositoryMock, times(1)).store(travelMock);
-//	 }
+	@Test
+	public void editTravelCallsProcessorMethodCorrectly() throws Exception {
+		when(travelViewModelMock.getDistanceTravelled()).thenReturn(DISTANCE_TRAVELLED);
+		when(travelViewModelMock.getVehicle()).thenReturn(USER_VEHICLE.toString());
+		when(travelViewModelMock.getDate()).thenReturn(TRAVEL_DATE);
+		when(travelViewModelMock.getUserEmail()).thenReturn(VALID_EMAIL);
+		when(travelViewModelMock.getComment()).thenReturn(COMMENT);
+		
+		travelService.editTravel(TRAVEL_UID, travelViewModelMock);
+		
+		verify(travelProcessorMock, times(1)).editTravel(TRAVEL_UID, DISTANCE_TRAVELLED, USER_VEHICLE.toString(), new LocalDate(TRAVEL_DATE), VALID_EMAIL, COMMENT);
+	}
+	
+	@Test
+	public void createTravelCallsProcessorMethodCorrectly() throws Exception {
+		when(travelViewModelMock.getDistanceTravelled()).thenReturn(DISTANCE_TRAVELLED);
+		when(travelViewModelMock.getVehicle()).thenReturn(USER_VEHICLE.toString());
+		when(travelViewModelMock.getDate()).thenReturn(TRAVEL_DATE);
+		when(travelViewModelMock.getUserEmail()).thenReturn(VALID_EMAIL);
+		when(travelViewModelMock.getComment()).thenReturn(COMMENT);
+		
+		travelService.createTravel(travelViewModelMock);
+		
+		verify(travelProcessorMock, times(1)).createTravel(DISTANCE_TRAVELLED, USER_VEHICLE.toString(), new LocalDate(TRAVEL_DATE), VALID_EMAIL, COMMENT);
+	}
+	
+	@Test
+	public void retrieveUserTravelViewModelsForCurrentPayPeriodCallsConverterMethod() {
+		travelService.retrieveUserTravelViewModelsForCurrentPayPeriod(VALID_EMAIL);
+		verify(travelConverterMock, times(1)).convert(travels);
+	}
+	
+	@Test
+	public void retrieveTravelViewModelReturnsValidViewModel() throws TravelNotFoundException {
+		when(travelProcessorMock.retrieveTravelByUid(TRAVEL_UID)).thenReturn(travelMock);
+		when(travelConverterMock.convert(travelMock)).thenReturn(travelViewModelMock);
+		TravelViewModel returnedViewModel = travelService.retrieveTravelViewModel(TRAVEL_UID);
+		assertEquals(returnedViewModel, travelViewModelMock);
+	}
+	
 }
