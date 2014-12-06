aac94cb6525aeb25e3de946d07cf7994efa7db2d
Marie-Pier Pouliot-Lavoie <plavoie.mariepier@gmail.com>
2014-12-06 13:25:43 -0500

    TimeServiceTest fully completed. Services 100% tested

--- src/test/java/ca/ulaval/glo4003/appemployee/services/TimeServiceTest.java
+++ src/test/java/ca/ulaval/glo4003/appemployee/services/TimeServiceTest.java
@@ -1,134 +1,202 @@
-//package ca.ulaval.glo4003.appemployee.services;
-//
-//import static org.junit.Assert.assertNotNull;
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertTrue;
-//import static org.mockito.BDDMockito.given;
-//import static org.mockito.Mockito.doThrow;
-//import static org.mockito.Mockito.times;
-//import static org.mockito.Mockito.verify;
-//import static org.mockito.Mockito.when;
-//
-//import org.joda.time.LocalDate;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//import org.mockito.runners.MockitoJUnitRunner;
-//
-//import ca.ulaval.glo4003.appemployee.domain.exceptions.PayPeriodNotFoundException;
-//import ca.ulaval.glo4003.appemployee.domain.repository.PayPeriodRepository;
-//import ca.ulaval.glo4003.appemployee.domain.repository.TimeEntryRepository;
-//import ca.ulaval.glo4003.appemployee.domain.time.PayPeriod;
-//import ca.ulaval.glo4003.appemployee.domain.time.TimeEntry;
-//import ca.ulaval.glo4003.appemployee.domain.time.TimeProcessor;
-//import ca.ulaval.glo4003.appemployee.web.converters.TimeConverter;
-//import ca.ulaval.glo4003.appemployee.web.viewmodels.TimeViewModel;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public class TimeServiceTest {
-//
-//	private static final LocalDate CURRENT_DATE = new LocalDate();
-//	private static final String A_UID = "0001";
-//	private static final int PAYPERIOD_DURATION = 13;
-//
-//	@Mock
-//	private PayPeriodRepository payPeriodRepositoryMock;
-//
-//	@Mock
-//	private TimeEntryRepository timeEntryRepositoryMock;
-//
-//	@Mock
-//	private PayPeriod payPeriodMock;
-//
-//	@Mock
-//	private PayPeriod previousPayPeriodMock;
-//
-//	@Mock
-//	private TimeViewModel timeViewModelMock;
-//
-//	@Mock
-//	private TimeConverter timeConverterMock;
-//
-//	@Mock
-//	private TimeEntry timeEntryMock;
-//
-//	@Mock
-//	private UserService userServiceMock;
-//
-//	@Mock
-//	private TimeProcessor timeProcessorMock;
-//
-//	@InjectMocks
-//	private TimeService payPeriodService;
-//
-//	@Before
-//	public void setUp() {
-//		MockitoAnnotations.initMocks(this);
-//		payPeriodService = new TimeService(timeProcessorMock,
-//				timeConverterMock, userServiceMock);
-//	}
-//
-//	@Test
-//	public void canInstantiateService() {
-//		assertNotNull(payPeriodService);
-//	}
-//
-//	 public void
-//	 retrieveCurrentPayPeriodThrowsExceptionWhenPayPeriodNotFound() {
-//	 when(payPeriodRepositoryMock.findByDate(CURRENT_DATE)).thenReturn(null);
-//	 payPeriodService.retrieveCurrentPayPeriod();
-//	 }
-//
-//	 @Test
-//	 public void retrieveCurrentPayPeriodFindsPayPeriodWhenExists() {
-//	 when(payPeriodRepositoryMock.findByDate(CURRENT_DATE)).thenReturn(payPeriodMock);
-//	 PayPeriod currentPayPeriod = payPeriodService.retrieveCurrentPayPeriod();
-//	 assertEquals(payPeriodMock, currentPayPeriod);
-//	 }
-//
-//	 @Test(expected = PayPeriodNotFoundException.class)
-//	 public void
-//	 retrievePreviousPayPeriodThrowsExceptionWhenCurrentPayPeriodNotFound() {
-//	 when(payPeriodRepositoryMock.findByDate(CURRENT_DATE)).thenReturn(null);
-//	 payPeriodService.retrievePreviousPayPeriod();
-//	 }
-//
-//	 @Test
-//	 public void retrievePreviousPayPeriodCallsRetrieveCurrentPayPeriod() {
-//	 when(payPeriodRepositoryMock.findByDate(CURRENT_DATE)).thenReturn(payPeriodMock);
-//	 LocalDate startDate = new
-//	 LocalDate(CURRENT_DATE.minusDays(PAYPERIOD_DURATION));
-//	 when(payPeriodMock.getStartDate()).thenReturn(startDate);
-//	 when(payPeriodRepositoryMock.findByDate(startDate.minusDays(1))).thenReturn(previousPayPeriodMock);
-//	
-//	 PayPeriod previousPayPeriod =
-//	 payPeriodService.retrievePreviousPayPeriod();
-//	
-//	 assertEquals(previousPayPeriodMock, previousPayPeriod);
-//	 }
-//
-//	 @Test(expected = PayPeriodNotFoundException.class)
-//	 public void updatePayPeriodThrowsExceptionWhenPayPeriodNotFound() throws
-//	 Exception {
-//	 doThrow(PayPeriodNotFoundException.class).when(payPeriodRepositoryMock).update(payPeriodMock);
-//	 payPeriodService.editPayPeriod(payPeriodMock);
-//	 }
-//	
-//	 @Test
-//	 public void updatePayPeriodCallsPayPeriodRepository() throws Exception {
-//	 payPeriodService.updatePayPeriod(payPeriodMock);
-//	 verify(payPeriodRepositoryMock, times(1)).update(payPeriodMock);
-//	 }
-//
-//	 @Test
-//	 public void saveTimeEntryCallsTimeEntryRepository() throws Exception {
-//	 when(timeConverterMock.convert(timeViewModelMock)).thenReturn(timeEntryMock);
-//	 when(timeEntryMock.getUid()).thenReturn(A_UID);
-//	 payPeriodService.createTimeEntry(timeViewModelMock, payPeriodMock);
-//	 verify(timeEntryRepositoryMock, times(1)).store(any(TimeEntry.class));
-//	 }
-//
-//}
+package ca.ulaval.glo4003.appemployee.services;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.joda.time.LocalDate;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import ca.ulaval.glo4003.appemployee.domain.exceptions.TimeEntryNotFoundException;
+import ca.ulaval.glo4003.appemployee.domain.task.Task;
+import ca.ulaval.glo4003.appemployee.domain.task.TaskProcessor;
+import ca.ulaval.glo4003.appemployee.domain.time.PayPeriod;
+import ca.ulaval.glo4003.appemployee.domain.time.TimeEntry;
+import ca.ulaval.glo4003.appemployee.domain.time.TimeProcessor;
+import ca.ulaval.glo4003.appemployee.web.converters.TimeConverter;
+import ca.ulaval.glo4003.appemployee.web.viewmodels.PayPeriodViewModel;
+import ca.ulaval.glo4003.appemployee.web.viewmodels.TimeEntryViewModel;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TimeServiceTest {
+	
+	private List<Task> tasks = new ArrayList<Task>();
+	private List<TimeEntry> timeEntries = new ArrayList<TimeEntry>();
+	private Collection<TimeEntryViewModel> timeEntryModels = new ArrayList<TimeEntryViewModel>();
+
+	private static final String DATE = "2014-11-11";
+	private static final String END_DATE = "2014-11-23";
+	private static final String USER_EMAIL = "email@email.com";
+	private static final String COMMENT = "comment";
+	private static final String A_UID = "0001";
+	private static final double HOURS = 8.0;
+	private static final String TASK_NAME = "name";
+
+	@Mock
+	private PayPeriod payPeriodMock;
+
+	@Mock
+	private PayPeriodViewModel timeViewModelMock;
+	
+	@Mock
+	private TimeEntryViewModel timeEntryViewModelMock;
+
+	@Mock
+	private TimeConverter timeConverterMock;
+
+	@Mock
+	private TimeEntry timeEntryMock;
+
+	@Mock
+	private UserService userServiceMock;
+
+	@Mock
+	private TimeProcessor timeProcessorMock;
+	
+	@Mock
+	private TaskProcessor taskProcessorMock;
+
+	@InjectMocks
+	private TimeService timeService;
+
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		timeService = new TimeService(taskProcessorMock, timeProcessorMock,
+				timeConverterMock);
+	}
+
+	@Test
+	public void canInstantiateService() {
+		assertNotNull(timeService);
+	}
+	
+	@Test
+	public void retrieveCurrentPayPeriodReturnsValidPayPeriod() {
+		when(timeProcessorMock.retrieveCurrentPayPeriod()).thenReturn(payPeriodMock);
+		PayPeriod returnedPayPeriod = timeService.retrieveCurrentPayPeriod();
+		assertEquals(payPeriodMock, returnedPayPeriod);
+	}
+	
+	@Test
+	public void retrievePreviousPayPeriodReturnsValidPayPeriod() {
+		when(timeProcessorMock.retrievePreviousPayPeriod()).thenReturn(payPeriodMock);
+		PayPeriod returnedPayPeriod = timeService.retrievePreviousPayPeriod();
+		assertEquals(payPeriodMock, returnedPayPeriod);
+	}
+	
+	@Test
+	public void editPayPeriodCallsCorrectProcessorMethod() throws Exception {
+		timeService.editPayPeriod(payPeriodMock);
+		verify(timeProcessorMock, times(1)).editPayPeriod(payPeriodMock);
+	}
+	
+	@Test
+	public void createTimeEntryCallsCorrectProcessorMethod() throws Exception {
+		when(timeEntryViewModelMock.getHours()).thenReturn(HOURS);
+		when(timeEntryViewModelMock.getDate()).thenReturn(DATE);
+		when(timeEntryViewModelMock.getTaskId()).thenReturn(A_UID);
+		when(timeEntryViewModelMock.getUserEmail()).thenReturn(USER_EMAIL);
+		when(timeEntryViewModelMock.getComment()).thenReturn(COMMENT);
+		
+		timeService.createTimeEntry(timeEntryViewModelMock, payPeriodMock);
+		
+		verify(timeProcessorMock, times(1)).createTimeEntry(payPeriodMock, HOURS, new LocalDate(DATE), USER_EMAIL, A_UID, COMMENT);
+	}
+	
+	@Test
+	public void updateTimeEntryCallsCorrectProcessorMethod() throws Exception {
+		when(timeEntryViewModelMock.getHours()).thenReturn(HOURS);
+		when(timeEntryViewModelMock.getDate()).thenReturn(DATE);
+		when(timeEntryViewModelMock.getTaskId()).thenReturn(A_UID);
+		when(timeEntryViewModelMock.getUserEmail()).thenReturn(USER_EMAIL);
+		when(timeEntryViewModelMock.getComment()).thenReturn(COMMENT);
+		when(timeEntryViewModelMock.getUid()).thenReturn(A_UID);
+		
+		timeService.updateTimeEntry(timeEntryViewModelMock);
+		
+		verify(timeProcessorMock, times(1)).editTimeEntry(A_UID, HOURS, new LocalDate(DATE), USER_EMAIL, A_UID, COMMENT);
+	}
+	
+	@Test
+	public void retrieveCurrentPayPeriodViewModelReturnsPayPeriodViewModel() {
+		when(timeService.retrieveCurrentPayPeriod()).thenReturn(payPeriodMock);
+		when(payPeriodMock.getStartDate()).thenReturn(new LocalDate(DATE));
+		when(payPeriodMock.getEndDate()).thenReturn(new LocalDate(END_DATE));
+		
+		PayPeriodViewModel returnedPayPeriodModel = timeService.retrieveCurrentPayPeriodViewModel();
+		
+		assertNotNull(returnedPayPeriodModel);
+	}
+	
+	@Test
+	public void retrievePreviousPayPeriodViewModelReturnsPayPeriodViewModel() {
+		when(timeService.retrievePreviousPayPeriod()).thenReturn(payPeriodMock);
+		when(payPeriodMock.getStartDate()).thenReturn(new LocalDate(DATE));
+		when(payPeriodMock.getEndDate()).thenReturn(new LocalDate(END_DATE));
+		
+		PayPeriodViewModel returnedPayPeriodModel = timeService.retrievePreviousPayPeriodViewModel();
+		
+		assertNotNull(returnedPayPeriodModel);
+	}
+	
+	@Test
+	public void retrieveTimeEntryViewModelForUserReturnsValidTimeEntryViewModel() {
+		when(taskProcessorMock.retrieveAllTasksByUserId(USER_EMAIL)).thenReturn(tasks);
+		when(timeConverterMock.convert(USER_EMAIL, tasks)).thenReturn(timeEntryViewModelMock);
+		
+		TimeEntryViewModel returnedModel = timeService.retrieveTimeEntryViewModelForUser(USER_EMAIL);
+		
+		assertEquals(timeEntryViewModelMock, returnedModel);
+	}
+	
+	@Test
+	public void retrieveAllTimeEntriesViewModelsForCurrentPayPeriodReturnsCollectionOfTimeEntryViewModels() throws TimeEntryNotFoundException {
+		when(timeService.retrieveCurrentPayPeriod()).thenReturn(payPeriodMock);
+		when(timeProcessorMock.evaluateUserTimeEntriesForPayPeriod(payPeriodMock, USER_EMAIL)).thenReturn(timeEntries);
+		when(timeConverterMock.convert(timeEntries)).thenReturn(timeEntryModels);
+		
+		Collection<TimeEntryViewModel> returnedModels = timeService.retrieveAllTimeEntriesViewModelsForCurrentPayPeriod(USER_EMAIL);
+		
+		assertEquals(timeEntryModels, returnedModels);
+	}
+	
+	@Test
+	public void retrieveAllTimeEntriesViewModelsForPreviousPayPeriodReturnsCollectionOfTimeEntryViewModels() throws TimeEntryNotFoundException {
+		when(timeService.retrievePreviousPayPeriod()).thenReturn(payPeriodMock);
+		when(timeProcessorMock.evaluateUserTimeEntriesForPayPeriod(payPeriodMock, USER_EMAIL)).thenReturn(timeEntries);
+		when(timeConverterMock.convert(timeEntries)).thenReturn(timeEntryModels);
+		
+		Collection<TimeEntryViewModel> returnedModels = timeService.retrieveAllTimeEntriesViewModelsForPreviousPayPeriod(USER_EMAIL);
+		
+		assertEquals(timeEntryModels, returnedModels);
+	}
+	
+	@Test
+	public void retrieveTimeEntryViewModelReturnsValidViewModel() throws TimeEntryNotFoundException {
+		when(timeProcessorMock.retrieveTimeEntryByUid(A_UID)).thenReturn(timeEntryMock);
+		when(timeEntryMock.getUserEmail()).thenReturn(USER_EMAIL);
+		when(taskProcessorMock.retrieveAllTasksByUserId(USER_EMAIL)).thenReturn(tasks);
+		when(timeEntryMock.getTaskUid()).thenReturn(A_UID);
+		when(taskProcessorMock.retrieveTaskName(A_UID)).thenReturn(TASK_NAME);
+		when(timeConverterMock.convert(timeEntryMock, TASK_NAME, tasks)).thenReturn(timeEntryViewModelMock);
+		
+		TimeEntryViewModel returnedModel = timeService.retrieveTimeEntryViewModel(A_UID);
+		
+		assertEquals(timeEntryViewModelMock, returnedModel);
+	}
+
+
+}
